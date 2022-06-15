a451fb01e406e6c0b9cb70e2f5526e5a7249673f
Angelo Masiero <angelomasiero@protonmail.com>
2022-06-15 17:14:29 -0300

    Added Galery, Animations, and other changes

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+	document.getElementById("loading").style.display = "none";
+	gsap.from("#logo-frame", {opacity: 0, ease: "none", duration: 2})
+})
+
+
